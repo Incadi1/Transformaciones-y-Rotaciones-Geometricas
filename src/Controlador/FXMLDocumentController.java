c306f00364ea71cdd4231b865fad611d707efa56
Ingrid Carmona Diaz <>
2018-09-20 13:05:40 -0500

    Se hacen los parametros respectivos a cada objeto

--- src/Controlador/FXMLDocumentController.java
+++ src/Controlador/FXMLDocumentController.java
@@ -1,155 +1,155 @@
+package Controlador;
 
+/**
+ * Descripción de la clase ...
+ *
+ * @author ingrid.carmona@uao.edu.co , Ingrid Carmona Diaz , Código 2150147
+ * @author sebastian.carmona@uao.edu.co , Sebastian Carmona , Código 2170266
+ * @date 21 Septiembre 2018 - Viernes
+ * @version 8.2
+ */
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.animation.Animation;
-import javafx.animation.KeyFrame;
+import javafx.animation.RotateTransition;
 import javafx.animation.Timeline;
-import javafx.application.Platform;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-
 import javafx.fxml.FXML;
 import javafx.geometry.Point3D;
 import javafx.scene.control.MenuButton;
-import javafx.scene.control.Slider;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Box;
 import javafx.scene.shape.Cylinder;
+import javafx.scene.shape.Sphere;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import javax.swing.JOptionPane;
 
-public class FXMLDocumentController {
-   
+public class FXMLDocumentController extends TransformacionRotacion {
+
     private Stage stage;
-    private Slider slider;
 
     @FXML
     private MenuButton MbAcciones;
 
     @FXML
     private Pane Panel;
-    
+
     @FXML
     private MenuButton MbAnimar;
-    
+
     @FXML
     private MenuButton MbDetener;
 
+    private Box Cubo;
     private Cylinder Cyl;
-    private Timeline Time;
-    private int ContBoxTras;
-    private int ContBoxRot1;
-    private int ContBoxRot2;
-    private int SignoBoxTras;
-    private int SignoBoxRot1;
-    private boolean ResetBoxRot1;
-    private int SignoBoxRot2;
-    private boolean ResetBoxRot2;
+    private Sphere Sph;
 
-    /**
-     * Inicializa los elementos del Controller.
-     *
-     * @param url la ubicacion utilizada para resolver rutas de acceso relativas
-     * al objeto raíz o null si no se conoce la ubicacion.
-     * @param rb los recursos utilizados para localizar el objeto raíz o null si
-     * el objeto raíz no fue localizado.
-     */
+    private Timeline time;
 
-    public void initialize(URL url, ResourceBundle rb) {
-      
-        ContBoxTras = -200;
-        ContBoxRot1 = 0;
-        ContBoxRot2 = 0;
-        SignoBoxTras = 0;
-        SignoBoxRot1 = 0;
-        SignoBoxRot2 = 0;
-        ResetBoxRot1 = false;
-        ResetBoxRot2 = false;
 
-        Time = new Timeline(new KeyFrame(Duration.millis(10), new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-
-            }
-        }));
-        Time.setCycleCount(Animation.INDEFINITE);
-
-        Time.play();
-
-    }
 
     public void setStage(Stage stage) {
-        this.stage = stage;
-    }
-
+        this.stage = stage;    }
+    
     public Pane getPanel() {
-        return Panel;
+        return Panel;  }
+
+    public void setObjetoGeometricoCyl(Cylinder Cyl) {
+       this.Cyl = Cyl;
+    }
+    
+    public void setObjetoGeometricoBox(Box Cubo){
+        this.Cubo = Cubo;
+    }
+    
+    public void setObjetoGeometrico(Sphere Sph){
+        this.Sph = Sph;
     }
 
+//-------------------------------------
     @FXML
-    public void handleDraggedSliderAngle() {
-//        resetBoxRot1 = false;
-//        bb3.setRotate(slAngle.getValue());        
-    }
-
-    /**
-     * Activa el proceso de reseteo del cubo bb3.
-     */
-    @FXML
-    public void handleMouseRealeasedSlider() throws InterruptedException {
-        ResetBoxRot1 = true;
-    }
-
-    /**
-     * Rota el cubo bb4 al activarse.
-     */
-    @FXML
-    public void handleDraggedSliderAngle1() {
-        ResetBoxRot2 = false;
+    void BtEsfera(ActionEvent event) {
+//Mostrar el esfera en pantalla y emppieza a girar
+        TraslacionEsfera();
 
     }
 
-    /**
-     * Activa el proceso de reseteo del cubo bb4.
-     */
+ 
+
     @FXML
-    public void handleMouseRealeasedSlider1() {
-        ResetBoxRot2 = true;
+    public void BtCubo(ActionEvent event) {
+        //Mostrar el cubo en pantalla
+    }
+
+    @FXML
+    void BtCilindro(ActionEvent event) {
+        //Empieze a aparecer aleatorio cuando es presionado
+    }
+    //--------------------------------
+
+    @FXML
+    void MbAcciones(ActionEvent event) {
+
+    }
+
+    //-------- Cubo-----
+    @FXML
+    void MbAnimarCubo(ActionEvent event) {
+        RotateTransition rot = new RotateTransition(Duration.seconds(3), Cubo);
+        rot.setCycleCount(Animation.INDEFINITE);
+        rot.setFromAngle(360);
+        rot.setToAngle(-360);
+        rot.setAxis(new Point3D(Math.random(), 1, Math.random()));
+        rot.play();
+    }
+
+    @FXML
+    void MbDetener(ActionEvent event) {
+        time.stop();
+    }
+
+    //---------------------
+    @FXML
+    void MbarInfo(ActionEvent event) {
+
+        JOptionPane.showMessageDialog(null, "Dar click en las figuras para empezar a dar el moviemiento/n"+" en el caso de el boton cubo en acciones se da click/n "
+                + "animar para empezar a generar el movimiento");
+    }
+
+    @FXML
+    void MbarCreadores(ActionEvent event) {
+ JOptionPane.showMessageDialog(null,"Sebastian Carmona " + "Ingrid Carmona Diaz");
+    }
+
+    @FXML
+    void MbarOpciones(ActionEvent event) {
+        MbarLimpiar(event);
+        MbarSalir(event);
+    }
+
+    @FXML
+    void MbarLimpiar(ActionEvent event) { // DUDA
+        double w = Panel.getWidth();
+        double h = Panel.getHeight();
+
     }
 
     /**
      * Detiene el timeline y termina la aplicación.
      */
     @FXML
-    public void handleButtonTerminar() {
-        Time.stop();
-        Platform.exit();
+    void MbarSalir(ActionEvent event) {
+        System.exit(0);
     }
 
-    public void setObjetoGeometrico5(Cylinder Cylinde) {
-        Cylinde.setTranslateX(200);
-        Cylinde.setTranslateY(100);
-        Cylinde.setTranslateZ(-250);
-        this.Cyl = Cylinde;
-        Cylinde.setRotationAxis(new Point3D(-1, -1, 1));
-    }
-//-------------------------------------
-    @FXML
-    void BtCubo(ActionEvent event) {
-
-    }
-
-    @FXML
-    void BtEsfera(ActionEvent event) {
-
-    }
-
-    @FXML
-    void BtCilindro(ActionEvent event) {
-
-    }
-
-    @FXML
-    void MbAcciones(ActionEvent event) {
+    public void initialize(URL url, ResourceBundle rb) {
+    //    Panel = Pane.getPoint3D();
+        Cubo.setVisible(false);
+        Sph.setVisible(false);
+        Cyl.setVisible(false);
+        double w = Panel.getWidth();
+        double h = Panel.getHeight();
 
     }
 
--- src/Controlador/TransformacionRotacion.java
+++ src/Controlador/TransformacionRotacion.java
@@ -1,75 +1,143 @@
 /**
-* Descripción de la clase ...
-* @author ingrid.carmona@uao.edu.co , Ingrid Carmona Diaz , Código 2150147  
-* @author sebastian.carmona@uao.edu.co , Sebastian Carmona , Código 2170266
-* @date 21 Septiembre 2018 - Viernes
-* @version 8.2
-*/
+ * Descripción de la clase ...
+ *
+ * @author ingrid.carmona@uao.edu.co , Ingrid Carmona Diaz , Código 2150147
+ * @author sebastian.carmona@uao.edu.co , Sebastian Carmona , Código 2170266
+ * @date 21 Septiembre 2018 - Viernes
+ * @version 8.2
+ */
 package Controlador;
 
 import Utilidades.ObjectFactory;
 import Utilidades.PanelFactory;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.geometry.Point3D;
 import javafx.scene.AmbientLight;
 import javafx.scene.Group;
 import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Box;
 import javafx.scene.shape.Cylinder;
+import javafx.scene.shape.Sphere;
 import javafx.stage.Stage;
-
+import javafx.util.Duration;
 
 public class TransformacionRotacion extends Application {
+
+    private Box Cubo;
+    private Cylinder Cyl;
+    private Sphere Sph;
     
-    Cylinder Cyl;
-    
+    int contTraslacion = 0;
+    int sigTraslacion = 0;
+    int sigRotacion = 0;
+    int angulo = 360;
+
+    int conTraslascion = -100;
+    int Sigtraslacion = 0;
+
     @Override
     public void start(Stage stage) throws Exception {
         
-         //creación del cubo
+
+        //creación del Cilindro 
         double lado = 100;
         Cyl = new Cylinder(200, 200);
-            //material
-        Image imageDiffuseAjedrez = new Image(getClass().getResource("/Texturas/Cubo.jpg").toExternalForm());
-       
-        
-        PhongMaterial material5 = new PhongMaterial();
-        material5.setBumpMap(imageDiffuseAjedrez);
-        material5.setDiffuseMap(imageDiffuseAjedrez);
-           
-        Cyl.setMaterial(material5);
-        
-        Node controlPanel = PanelFactory.createControlPanel(stage, Cyl);
-        
-        Node scene3D5 = ObjectFactory.createScene3D(Cyl);
+        Cyl.setTranslateX(200);
+        Cyl.setTranslateY(100);
+        Cyl.setTranslateZ(-250);
+        Cyl.setRotationAxis(new Point3D(-1, -1, 1));
+        //material
 
         
+          //Creacion de la esfera y tiempo de rotación
+        Sph = new Sphere(100);
+        Sph.setTranslateX(100);
+        Sph.setTranslateY(-100);
+        Sph.setTranslateZ(0);
+        Sph.setRotationAxis(new Point3D(0, 1, 0));
+
+        Timeline tm = new Timeline(new KeyFrame(Duration.seconds(0.003), ae -> TraslacionEsfera()));
+        tm.setCycleCount(Animation.INDEFINITE);
+        tm.play();
+
+        //Creacion del cubo y tiempo de rotacion 
+        Cubo = new Box(50, 50, 50);
+        Cubo.setTranslateX(0);
+        Cubo.setTranslateY(0);
+        Cubo.setTranslateZ(0);
+        Sph.setRotationAxis(new Point3D(1, 0, 0));
+
+
+        //-----TEXTURAS PARA EL CUBO-------
+        Image TexturasCubo = new Image(getClass().getResource("/Texturas/Cubo.jpg").toExternalForm());
+        PhongMaterial material5 = new PhongMaterial();
+        material5.setBumpMap(TexturasCubo);
+        material5.setDiffuseMap(TexturasCubo);
+        Cubo.setMaterial(material5);
+        
+          //-----TEXTURAS PARA EL CILINDRO-------
+         Image TexturasCylinder = new Image(getClass().getResource("/Texturas/Cilindro.png").toExternalForm());
+        PhongMaterial material6 = new PhongMaterial();
+        material6.setBumpMap(TexturasCylinder);
+        material6.setDiffuseMap(TexturasCylinder);
+        Cyl.setMaterial(material6);
+        
+          //-----TEXTURAS PARA ELA ESFERA-------
+         Image TexturasSphere = new Image(getClass().getResource("/Texturas/Esfera.png").toExternalForm());
+        PhongMaterial material7 = new PhongMaterial();
+        material7.setBumpMap(TexturasSphere);
+        material7.setDiffuseMap(TexturasSphere);
+        Sph.setMaterial(material7);
+
+        Node controlPanel = PanelFactory.createControlPanel(stage, Cyl, Cubo, Sph);
+
+        Node scene3D5 = ObjectFactory.createScene3D(Cubo);
+        Node scene3D6 = ObjectFactory.createScene3D(Sph);
+        Node scene3D7 = ObjectFactory.createScene3D(Cyl);
     
-        AmbientLight luz = new AmbientLight();     //luces (cree un AmbientLight)
+        
+        
+//-----------------CREA LAS LUCES Y SE AGREGAN LOS HIJIOS --------------------
+        AmbientLight luz = new AmbientLight(Color.WHITE);     //luces (cree un AmbientLight)
         Group root = new Group();                          //Group
         root.getChildren().add(controlPanel);           //agregue al root la scene3D como un hijo
         root.getChildren().add(scene3D5);               //agregue al root el controlPanel como un hijo
+        root.getChildren().add(scene3D6);               //agregue al root el controlPanel como un hijo
+        root.getChildren().add(scene3D7);               //agregue al root el controlPanel como un hijo
         root.getChildren().add(luz);                         //agregue al root la luz como un hijo
-       
-        
+//----------------------------------------------------------------------------
         
         Scene scene = new Scene(root);
-
-        //Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.getScene().setCamera(new PerspectiveCamera());
         stage.setResizable(false);
         stage.setTitle("Rotacion General");
         stage.show();
+
+
+    }
+    
+       public void TraslacionEsfera() {
+
+        if (conTraslascion == -100) {
+            Sigtraslacion = 1;
+        } else if (conTraslascion == 300) {
+            Sigtraslacion = -1;
+        }
+        Sph.setTranslateX(conTraslascion);
+        conTraslascion += Sigtraslacion;
     }
 
-   
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
